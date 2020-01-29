5088c54aabfae8cbceb77d86cb812a779b286d23
Ashley <burning.rose85@gmail.com>
2020-01-29 14:53:56 -0500

    forget that serial number thing, just use ecm manufacturer data

--- assets/udev_linkcable_rules.sh
+++ assets/udev_linkcable_rules.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-linkserial=`udevadm info -a -n /dev/ttyUSB0 | grep '{serial}' | head -n1 | cut -d '"' -f2`
+linkserial=`udevadm info -a -n /dev/ttyUSB0 | grep '{manufacturer}' | head -n1 | cut -d '"' -f2`
 
 cat <<EOF > /etc/udev/rules.d/99-usb-serial.rules
 SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="$linkserial", SYMLINK+="ttyUSB98", OWNER="pi", GROUP="dialout"
