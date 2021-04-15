b5c32edc12095c33d873a51bd84faf392a8034fb
Ashley Young <asyoung@redhat.com>
2021-04-14 20:08:10 -0400

    updating udev rules and scripts.

--- assets/setECML_baud.sh
+++ assets/setECML_baud.sh
@@ -1,12 +1,11 @@
-#/bin/sh
+#!/bin/sh
 
-# define the path/file of the ecmlink cable after udev changes are made
-FILE=/dev/ttyUSB0
+logger -p local0.notice -t ${0##*/}[$$] "Setting ECMLink Cable usbTTY0 Baud Rate"
 
-# if the cable is there, and udev rules worked, then set the speed.
-if [ -f "$FILE" ]; then
-    setserial /dev/ttyUSB0 baud_base 3000000 divisor 192 spd_cust
-# otherwise, just exit with some informative text
-else 
-    echo "$FILE not found \n Cable may not be inserted or uDev rules may not yet be loaded. \n If you're sure the cable is present, try a reboot."
+linkSet=$(setserial /dev/ttyUSB0 baud_base 3000000 divisor 192 spd_cust)
+
+if $linkSet ; then
+    logger -p local0.notice -t ${0##*/}[$$] "ECMLink Baud Rate Set Successfully"
+else
+    logger -p local0.notice -t ${0##*/}[$$] "Failed To Set ECMLink Baud Rate"
 fi
--- assets/udev_linkcable_rules.sh
+++ assets/udev_linkcable_rules.sh
@@ -1,21 +1,19 @@
-#/bin/sh
-
-linkserial=`udevadm info -a -n /dev/ttyUSB0 | grep '{manufacturer}' | head -n1 | cut -d '"' -f2`
-
-cat <<EOF > /etc/udev/rules.d/99-usb-serial.rules
-SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="$linkserial", SYMLINK+="ttyUSB98", OWNER="pi", GROUP="dialout", RUN+="assets/setECML_baud.sh"
-EOF
+#!/bin/sh
 
 ## Lets tell Linux to set the baud rate of ttyUSB98 every time the cable is plugged in
 ## Based on input from https://www.dsmtuners.com/threads/raspberry-pi-running-ecmlink-on-headunit.476635/
 ## and
 ## https://unix.stackexchange.com/questions/28548/how-to-run-custom-scripts-upon-usb-device-plug-in
-## KERNEL=="sd*", ATTRS{vendor}=="Yoyodyne", ATTRS{model}=="XYZ42", ATTRS{serial}=="123465789", RUN+="/pathto/script"
-## setserial /dev/ttyUSB98 baud_base 3000000 divisor 192 spd_cust
 
+cat <<EOF > /etc/udev/rules.d/99-usb-serial.rules
+
+SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{manufacturer}=="ECMTuning", SYMLINK+="ttyUSB98", OWNER="pi", GROUP="dialout", ENV{HOME}="/home/pi", RUN+="/opt/DSMPi/assets/setECML_baud.sh"
+
+EOF
+
+## Lock down the permissions on the rules
 chmod 644 /etc/udev/rules.d/99-usb-serial.rules
 chown root:root /etc/udev/rules.d/99-usb-serial.rules
 
-udevadm control –reload-rules
-#udevadm trigger /dev/ecmlink
+## Trigger the uDev rule update
 udevadm trigger /dev/ttyUSB0
